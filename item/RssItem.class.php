77c9c0d80e22f1794853b22665facc834ca34a17
mlntn <jared@mlntn.com>
2011-04-05 07:00:31 -0700

    Added parse method to stringify pubDate for RSS

--- item/RssItem.class.php
+++ item/RssItem.class.php
@@ -4,4 +4,10 @@ require_once 'BaseItem.class.php';
 
 class RssItem extends BaseItem {
 	
+	public function parse() {
+		parent::parse();
+		
+		$this->item['pubDate'] = (string) $this->item['pubDate'];
+	}
+	
 }
